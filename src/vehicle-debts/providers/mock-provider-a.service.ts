2abf670aa5e445694359508a0b6d572f84bbcbb2
Jonathan Rodrigues - Tec Leader <jonathanrodrigues12@gmail.com>
2026-06-14 16:41:26 -0300

    fix: update mock import and add bearer auth to swagger docs

--- src/vehicle-debts/providers/mock-provider-a.service.ts
+++ src/vehicle-debts/providers/mock-provider-a.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { IVehicleDebtsProvider } from '../interfaces/vehicle-debts-provider.interface';
 import { VehicleDebtsResponseDto } from '../dto/vehicle-debts-response.dto';
-import * as mockData from '../mocks/provider-a.mock.json';
+import mockData from '../mocks/provider-a.mock.json';
 
 @Injectable()
 export class MockProviderAService implements IVehicleDebtsProvider {
   async getDebts(plate: string): Promise<VehicleDebtsResponseDto | null> {
-    const record = (mockData as VehicleDebtsResponseDto[]).find(
+    const record = (mockData as unknown as VehicleDebtsResponseDto[]).find(
       (entry) => entry.vehicle.toUpperCase() === plate.toUpperCase(),
     );
     return record ?? null;
--- src/vehicle-debts/vehicle-debts.controller.ts
+++ src/vehicle-debts/vehicle-debts.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { VehicleDebtsService } from './vehicle-debts.service';
 import { VehicleDebtsResponseDto } from './dto/vehicle-debts-response.dto';
 
 @ApiTags('Vehicle Debts')
+@ApiBearerAuth()
 @Controller('vehicle-debts')
 export class VehicleDebtsController {
   constructor(private readonly vehicleDebtsService: VehicleDebtsService) {}
